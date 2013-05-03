80059e7261089dfcc486e43fd777f635a46821f9
jekk0s <jeroen.peelaerts@gmail.com>
2013-05-04 00:51:09 +0200

    Native Library + testcase updated

--- java/src/test/java/be/kuleuven/med/brainfuck/connector/test/ThorlabsLibraryTest.java
+++ java/src/test/java/be/kuleuven/med/brainfuck/connector/test/ThorlabsLibraryTest.java
@@ -1,16 +1,24 @@
 package be.kuleuven.med.brainfuck.connector.test;
 
-import junit.framework.TestCase;
-import be.kuleuven.med.brainfuck.connector.DC2100Library;
+import java.nio.FloatBuffer;
 
-import com.sun.jna.NativeLibrary;
+import junit.framework.TestCase;
+import be.kuleuven.med.brainfuck.connector.ThorlabsDC2100Library;
+import be.kuleuven.med.brainfuck.connector.ThorlabsDC2100Library.__stdcall;
+
+import com.sun.jna.WString;
+import com.sun.jna.ptr.NativeLongByReference;
 
 public class ThorlabsLibraryTest extends TestCase {
 
 	public void testLibrary() {
-		
-		NativeLibrary lib = DC2100Library.JNA_NATIVE_LIB;
-		
+		NativeLongByReference handle = new NativeLongByReference();
+		__stdcall result = ThorlabsDC2100Library.DC2100_init(new WString("ASRL4::INSTR"), 0, 0, handle);
+		FloatBuffer floatBuffer = FloatBuffer.allocate(16);
+		floatBuffer.put(200f);
+		long dc2100_getConstCurrent = ThorlabsDC2100Library.DC2100_getConstCurrent(handle.getValue(), floatBuffer);
+		System.out.println(dc2100_getConstCurrent);
+		System.out.println(result != null ? result.toString() : "null");
 	}
 	
 }
